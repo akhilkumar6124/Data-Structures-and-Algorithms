8e85d9a329a3d3e158f0eacbb0411cbe3141a1b2
akhilkumar6124 <paluriakhilkumar6124@gmail.com>
2026-01-02 14:28:29 +0530

    Search Algorithm

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,31 @@
+public class BinarySearch {
+    public static int binarySearch(int arr[], int key) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            }
+            if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int key = 7;
+        int result = binarySearch(arr, key);
+        if (result == -1) {
+            System.out.println("Element not found in the array");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
