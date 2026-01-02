237049f9d3f04335b8e06ea20fb8768d3a488585
akhilkumar6124 <paluriakhilkumar6124@gmail.com>
2026-01-02 15:13:53 +0530

    Sorting Algorithm

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Unsorted array:");
+        printArray(arr);
+        bubbleSort(arr);   
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}
---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println(); 
+    }
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Unsorted array:");
+        printArray(arr);
+        insertionSort(arr);   
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+}  
