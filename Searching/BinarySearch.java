3dcca270553e98131b359073d8b2923a2fff74d3
akhilkumar6124 <paluriakhilkumar6124@gmail.com>
2026-01-03 20:14:06 +0530

    modified...

--- Searching/BinarySearch.java
+++ Searching/BinarySearch.java
@@ -1,31 +1,34 @@
+import java.util.Arrays;
+
 public class BinarySearch {
-    public static int binarySearch(int arr[], int key) {
-        int left = 0;
-        int right = arr.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == key) {
-                return mid;
-            }
-            if (arr[mid] < key) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int key = 7;
-        int result = binarySearch(arr, key);
-        if (result == -1) {
-            System.out.println("Element not found in the array");
-        } else {
-            System.out.println("Element found at index: " + result);
-        }
-    }
+    public static int binearySearch(int arr[],int key) {
+		int left = 0;
+		int right = arr.length-1;
+		while(left<=right) {
+			int mid = (left+right)/2;
+			if(arr[mid]==key) {
+				return mid;
+			}else if(key>arr[mid]) {
+				left = mid + 1;
+			}else {
+				right = mid - 1;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		int arr[] = {2,4,6,8,1,3,5,7,9};
+		int key = 5;
+		Arrays.sort(arr);
+		for(int var : arr) {
+			System.out.print(var+" ");
+		}
+		System.out.println();
+		int result = binearySearch(arr,key);
+		if(result!=-1) {
+			System.out.println("element found at "+result);
+		}else {
+			System.out.println("element not found......");
+		}
+	}
 }
--- Searching/LinearSearch.java
+++ Searching/LinearSearch.java
@@ -1,21 +1,20 @@
 public class LinearSearch {
-    public static int linearSearch(int arr[], int key) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == key) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        int arr[] = { 1, 3, 5, 7, 9, 2, 4, 6, 8 };
-        int key = 7;
-        int result = linearSearch(arr, key);
-        if (result == -1) {
-            System.out.println("Element not found in the array");
-        } else {
-            System.out.println("Element found at index: " + result);
-        }
-    }
+    public static int linearSearch(int arr[],int key) {
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]==key) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static void main(String[] args) {
+		int arr[] = {2,4,6,8,1,3,5,7,9};
+		int key = 5;
+		int result = linearSearch(arr,key);
+		if(result!=-1) {
+			System.out.println("Element Found at "+result);
+		}else {
+			System.out.println("Element Not Found....");
+		}
+	}
 }
