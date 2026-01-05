8aac07beec6d7dfbfbfa8a7a154d90fda25027d1
akhilkumar6124 <paluriakhilkumar6124@gmail.com>
2026-01-05 12:37:09 +0530

    Modified MergeSort

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -1,53 +1,43 @@
+import java.util.Arrays;
+
 public class MergeSort{
-    public static void mergeSort(int arr[]){
-        int length = arr.length;
-        if(length<=1) return;
-        int middle = length/2;
-        int leftarray[] = new int[middle];
-        int rightarray[] = new int[length - middle];
-        int leftindex=0;
-        int rightindex=0;
-        for(int i=0; i<length;i++){
-            if(i<middle){
-                leftarray[leftindex++] = arr[i];
-            }else{
-                rightarray[rightindex++] = arr[i];
-            }
+    public static int[] mergeSort(int arr[]){
+        if(arr.length<=1){
+            return arr;
         }
-        mergeSort(leftarray);
-        mergeSort(rightarray);
-        merge(leftarray, rightarray, arr);
+        int mid = arr.length/2;
+        int leftarray[] = Arrays.copyOfRange(arr, 0, mid);
+        int rightarray[] = Arrays.copyOfRange(arr, mid, arr.length);
+
+        int sortedleftarray[] = mergeSort(leftarray);
+        int sortedrightarray[] = mergeSort(rightarray);
+
+        return merge(sortedleftarray,sortedrightarray);
     }
-    public static void merge(int[] leftarray, int[] rightarray, int[] arr){
-        int leftsize = leftarray.length;
-        int rightsize = rightarray.length;
-        int i=0,l=0,r=0;
-        while(l<leftsize && r<rightsize){
-            if(leftarray[l]<rightarray[r]){
-                arr[i] = leftarray[l];
-                i++;
-                l++;
+    public static int[] merge(int left[],int right[]){
+        int result[] = new int[left.length+right.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<left.length && j<right.length){
+            if(left[i]<right[j]){
+                result[k++] = left[i++];
             }else{
-                arr[i] = rightarray[r];
-                i++;
-                r++;
+                result[k++] = right[j++];
             }
         }
-        while(l<leftsize){
-            arr[i] = leftarray[l];
-            i++;
-            l++;
+        while(i<left.length){
+            result[k++] = left[i++];
         }
-        while(r<rightsize){
-            arr[i] = rightarray[r];
-            i++;
-            r++;
+        while(j<right.length){
+            result[k++] = right[j++];
         }
+        return result;
     }
     public static void main(String[] args) {
         int arr[] = {8,3,6,2,9,1,7,4,5,};
-        mergeSort(arr);
-        for(int var : arr){
+        int result[] = mergeSort(arr);
+        for(int var : result){
             System.out.print(var+" ");
         }
     }
