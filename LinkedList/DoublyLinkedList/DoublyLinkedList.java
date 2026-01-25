c5b3593f0e133f427d97aae2f899af2eafda14d9
akhilkumar6124 <paluriakhilkumar6124@gmail.com>
2026-01-25 22:58:40 +0530

    Modified Single and Double LinkedList...

--- LinkedList/DoublyLinkedList/DoublyLinkedList.java
+++ LinkedList/DoublyLinkedList/DoublyLinkedList.java
@@ -1,40 +1,51 @@
-public class DoublyLinkedList{
-    static class Node{
+public class DoublyLinkedList {
+    static class Node {
         int data;
         Node next;
         Node prev;
-        Node(int data){
+        Node(int data) {
             this.data = data;
             this.next = null;
             this.prev = null;
         }
     }
-    public static void main(String[] args){
-        Node fristNode = new Node(3);
-        Node secoundNode = new Node(6);
-        Node thridNode = new Node(9);
-        Node fourthNode = new Node(12);
-
-        fristNode.next = secoundNode;
-        secoundNode.next = thridNode;
-        thridNode.next = fourthNode;
-
-        fourthNode.prev = thridNode;
-        thridNode.prev = secoundNode;
-        secoundNode.prev = fristNode;
-
-        Node currentNode = fristNode;
-        System.out.println("Forward");
-        while(currentNode!=null){
-            System.out.print(currentNode.data+" ");
-            currentNode = currentNode.next;
+    private Node head;
+    private Node tail;
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+            return;
         }
-        System.out.println();
-        currentNode = fourthNode;
-        System.out.println("Backward");
-        while(currentNode!=null){
-            System.out.print(currentNode.data+" ");
-            currentNode = currentNode.prev;
+        tail.next = newNode;
+        newNode.prev = tail;
+        tail = newNode;
+    }
+    public void displayForward() {
+        System.out.print("Forward: ");
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " <-> ");
+            current = current.next;
         }
+        System.out.println("null");
+    }
+    public void displayBackward() {
+        System.out.print("Backward: ");
+        Node current = tail;
+        while (current != null) {
+            System.out.print(current.data + " <-> ");
+            current = current.prev;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.insertAtEnd(3);
+        list.insertAtEnd(6);
+        list.insertAtEnd(9);
+        list.insertAtEnd(12);
+        list.displayForward();
+        list.displayBackward();
     }
 }
--- LinkedList/SinglyLinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList/SinglyLinkedList.java
@@ -1,21 +1,45 @@
-public class SinglyLinkedList{
-    static class Node{
+public class SinglyLinkedList {
+    static class Node {
         int data;
         Node next;
-        Node(int data){
-            this.data =data;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
     }
-    public static void main(String[] args) {
-        Node head = new Node(5);
-        head.next = new Node(8);
-        head.next.next = new Node(11);
-        head.next.next.next = new Node(32);
-
-        Node currentNode = head;
-        while(currentNode!=null){
-            System.out.print(currentNode.data+" ");
-            currentNode = currentNode.next;
+    private Node head;
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
         }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insertAtEnd(5);
+        list.insertAtEnd(8);
+        list.insertAtEnd(11);
+        list.insertAtEnd(32);
+        list.insertAtBeginning(1);
+        list.display();
     }
 }
